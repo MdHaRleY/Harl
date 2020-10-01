8e5c3e3b534ca3659a1cc73db923db4dd4ace9b8
MdHaRleY <ttnarb0781@gmail.com>
2020-10-01 14:55:01 +0300

    first commit

--- upload3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-apt update;
-git init;
-git add *;
-git commit -m "first commit";
-git branch -M master;
-git remote add origin https://github.com/MdHaRleY/Harley.git;
-git push -u origin master
